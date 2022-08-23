12bd6a1f03e3fe88ed60e7cc8a22a6089dda93af
Samokhin Alexander <icorwin74@gmail.com>
2022-08-23 15:52:03 +0300

    checkout

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -1,13 +1,19 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
 
 
 def active_passcards_view(request):
     # Программируем здесь
-
-    all_passcards = Passcard.objects.all()
+    
+    in_da_storage = Visit.objects.filter(leaved_at=None)
+    for visit in in_da_storage:
+        enter_at = localtime(visit.entered_at)
+        print(f'Вошел в хранилище :{enter_at}')
+        print(f'Провел в хранилище: {localtime() - enter_at}')
+    active_passcards = Passcard.objects.filter(is_active=True)
     context = {
-        'active_passcards': all_passcards,  # люди с активными пропусками
+        'active_passcards': active_passcards,  # люди с активными пропусками
     }
     return render(request, 'active_passcards.html', context)
